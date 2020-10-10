883bbf0f07eaa13323a88fa37d9105f1be4d67c0
tranvanhung <hungitk17@gmail.com>
2020-10-10 09:37:03 +0700

    Update Code

--- Tour_du_lich/Controllers/HomeController.cs
+++ Tour_du_lich/Controllers/HomeController.cs
@@ -37,5 +37,59 @@ namespace Tour_du_lich.Controllers
             return View(khachList);
         }
 
+        public ActionResult QuanLyNhanVien()
+        {
+            DBTOUREntities DBNhanvien = new DBTOUREntities();
+            var nhanvienList = DBNhanvien.nhanviens.ToList();
+            return View(nhanvienList);
+        }
+
+        public ActionResult QuanLyGia()
+        {
+            DBTOUREntities DBGia = new DBTOUREntities();
+            var giaList = DBGia.gias.ToList();
+            return View(giaList);
+        }
+
+        [HttpGet]
+        public ActionResult QuanLyDiaDiem()
+        {
+            DBTOUREntities DBDiadiem = new DBTOUREntities();
+            var diadiemList = DBDiadiem.diadiems.ToList();
+            return View(diadiemList);
+        }
+        // POST: Home/Create
+        [HttpPost]
+        public ActionResult QuanLyDiaDiem(FormCollection collection)
+        {
+            try
+            {
+                //Method 1: Using Component Name  
+
+                ViewBag.madiadiem = collection["madiadiem"];
+                ViewBag.tendiadiem = collection["tendiadiem"];
+                DBTOUREntities DBTour = new DBTOUREntities();
+                diadiem d = new diadiem();
+                d.tendiadiem = collection["tendiadiem"];
+                d.madiadiem = collection["madiadiem"];
+                DBTour.diadiems.Add(d);
+                DBTour.SaveChanges();
+                return View("QuanLyDiaDiem");
+            }
+            catch
+            {
+                ViewBag.madiadiem = "ma dia diem";
+                ViewBag.tendiadiem = "ten dia diem";
+                return View();
+            }
+        }
+
+        public ActionResult QuanLyLoaiTour()
+        {
+            DBTOUREntities DBLoaitour = new DBTOUREntities();
+            var loaitourList = DBLoaitour.loaitours.ToList();
+            return View(loaitourList);
+        }
+
     }
 }
