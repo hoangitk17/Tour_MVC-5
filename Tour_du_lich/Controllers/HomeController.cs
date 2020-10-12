e8b3fe5c71b36dbd0714e984846317171fdb4404
Nguyen Van Hoang <hoangitk17@gmail.com>
2020-10-12 23:56:02 +0700

    separate file diadiem

--- Tour_du_lich/Controllers/HomeController.cs
+++ Tour_du_lich/Controllers/HomeController.cs
@@ -55,47 +55,7 @@ namespace Tour_du_lich.Controllers
 
 
 
-        [AcceptVerbs(HttpVerbs.Get)]
-        public ActionResult QuanLyDiaDiem()
-        {
-            ViewBag.name = "GET";
-            DBTOUREntities DBDiadiem = new DBTOUREntities();
-            var diadiemList = DBDiadiem.diadiems.ToList();
-            return View(diadiemList);
-        }
-        // POST: Home/Create
-        [AcceptVerbs(HttpVerbs.Post)]
-        public ActionResult QuanLyDiaDiem(diadiem d)
-        {
-            ViewBag.name = "POST";
-            try
-            {
-              
-                DBTOUREntities DBTour = new DBTOUREntities();
-                DBTour.diadiems.Add(d);
-                DBTour.SaveChanges();
-                DBTOUREntities DBDiadiem = new DBTOUREntities();
-                string message = "SUCCESS";
-                return Json(new { Message = message, JsonRequestBehavior.AllowGet });
-            }
-            catch(Exception ex)
-            {
-                string message = "FAIL";
-                return Json(new { Message = message, JsonRequestBehavior.AllowGet });
-            }
-        }
-
-        [AcceptVerbs(HttpVerbs.Post)]
-        public JsonResult getDiaDiem(string id)
-        {
-            
-            DBTOUREntities DBDiadiem = new DBTOUREntities();
-            DBDiadiem.Configuration.ProxyCreationEnabled = false;
-            List<diadiem> diadiemList = DBDiadiem.diadiems.ToList();
-            return Json(new {
-                diadiemList
-            }, JsonRequestBehavior.AllowGet);
-        }
+        
         public ActionResult QuanLyLoaiTour()
         {
             DBTOUREntities DBLoaitour = new DBTOUREntities();
