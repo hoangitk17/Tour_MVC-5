7a084c997d9af2bf2197a3490cccb6e63e799acb
tranvanhung <hungitk17@gmail.com>
2020-10-12 23:59:57 +0700

    update code

--- Tour_du_lich/Dao/GiaDao.cs
+++ Tour_du_lich/Dao/GiaDao.cs
@@ -2,10 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Tour_du_lich.Models;
 
 namespace Tour_du_lich.Dao
 {
     public class GiaDao
     {
+        DBTOUREntities DBTOUR = new DBTOUREntities();
+        public bool Update(string id, double giatien)
+        {
+            try
+            {
+                var gia = DBTOUR.gias.Find(id);
+                gia.giatien = giatien;
+                DBTOUR.SaveChanges();
+                return true;
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public gia GetById(string id)
+        {
+            return DBTOUR.gias.SingleOrDefault(x => x.magia == id);
+        }
     }
 }
