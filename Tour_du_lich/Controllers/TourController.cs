a16178b118db4bf9ea90772a4762ba216b8d0266
tranvanhung <hungitk17@gmail.com>
2020-10-16 22:33:34 +0700

    Update code

---
+++ Tour_du_lich/Controllers/TourController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tour_du_lich.Models;
+namespace Tour_du_lich.Controllers
+{
+    public class TourController : Controller
+    {
+        // GET: Tour
+        public ActionResult QuanLyTour()
+        {
+            DBTOUREntities DBTour = new DBTOUREntities();
+            var tourList = DBTour.tours.ToList();
+            return View(tourList);
+        }
+    }
+}
