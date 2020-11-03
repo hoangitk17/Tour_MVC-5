b79b04f531665ccc127dc1738628478bba82a632
tranvanhung <hungitk17@gmail.com>
2020-11-03 13:00:41 +0700

    Update Code

---
+++ Tour_du_lich/Models/ChiPhiDoan.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tour_du_lich.Models
+{
+    public class ChiPhiDoanModel
+    {
+        public String madoan { get; set; }
+        public String tenchiphi { get; set; }
+
+        public Nullable<double> gia { get; set; }
+        public Nullable<System.DateTime> ngaybatdau { get; set; }
+        public Nullable<System.DateTime> ngayketthuc { get; set; }
+
+
+        public ChiPhiDoanModel()
+        {
+
+        }
+
+        public ChiPhiDoanModel(String madoan, String tenchiphi, Nullable<double> gia, Nullable<System.DateTime> ngaybatdau, Nullable<System.DateTime> ngayketthuc )
+        {
+            this.madoan = madoan;
+            this.tenchiphi = tenchiphi;
+            this.gia = gia;
+            this.ngaybatdau = ngaybatdau;
+            this.ngayketthuc = ngayketthuc;
+        }
+        public ChiPhiDoanModel(ChiPhiDoanModel ChiPhiDoan)
+        {
+            this.madoan = ChiPhiDoan.madoan;
+            this.tenchiphi = ChiPhiDoan.tenchiphi;
+            this.gia = ChiPhiDoan.gia;
+            this.ngaybatdau = ChiPhiDoan.ngaybatdau;
+            this.ngayketthuc = ChiPhiDoan.ngayketthuc;
+        }
+    }
+}
---
+++ Tour_du_lich/Models/DoanhThuDoanModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tour_du_lich.Models
+{
+    public class DoanhThuDoanModel
+    {
+        public String madoan { get; set; }
+        public String makh { get; set; }
+        public String matour { get; set; }
+
+        public Nullable<double> gia { get; set; }
+        public Nullable<System.DateTime> ngaybatdau { get; set; }
+        public Nullable<System.DateTime> ngayketthuc { get; set; }
+
+        public int quantity_khach { get; set; }
+
+        public DoanhThuDoanModel()
+        {
+
+        }
+
+        public DoanhThuDoanModel(String madoan, String makh, String matour, Nullable<double> gia, Nullable<System.DateTime> ngaybatdau, Nullable<System.DateTime> ngayketthuc )
+        {
+            this.madoan = madoan;
+            this.matour = matour;
+            this.makh = makh;
+            this.gia = gia;
+            this.ngaybatdau = ngaybatdau;
+            this.ngayketthuc = ngayketthuc;
+        }
+        public DoanhThuDoanModel(DoanhThuDoanModel DoanhThuDoan)
+        {
+            this.madoan = DoanhThuDoan.madoan;
+            this.matour = DoanhThuDoan.matour;
+            this.makh = DoanhThuDoan.makh;
+            this.gia = DoanhThuDoan.gia;
+            this.ngaybatdau = DoanhThuDoan.ngaybatdau;
+            this.ngayketthuc = DoanhThuDoan.ngayketthuc;
+        }
+    }
+}
