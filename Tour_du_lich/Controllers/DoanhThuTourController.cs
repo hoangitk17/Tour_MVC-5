f69883f3b4dea3ffc60a6829a96fb06f18cf6c20
tranvanhung <hungitk17@gmail.com>
2020-11-14 15:00:20 +0700

    edit thong ke tour

--- Tour_du_lich/Controllers/DoanhThuTourController.cs
+++ Tour_du_lich/Controllers/DoanhThuTourController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -27,6 +28,7 @@ namespace Tour_du_lich.Controllers
                 ViewBag.thoigianbatdau = TempData["thoigianbatdau"];
                 ViewBag.thoigianketthuc = TempData["thoigianketthuc"];
                 ViewBag.quantity = TempData["quantity"];
+                ViewBag.quantitykhach = TempData["quantity_khach"];
             }
             if (Session["login"] != null)
             {
@@ -47,13 +49,31 @@ namespace Tour_du_lich.Controllers
 
                 List<DoanhThuTourModel> res = tour.GetDoanhThuTour(id_tour, thoigianbatdau, thoigianketthuc);
                 ViewBag.doanhthutours = res;
-                TempData["quantity"] = res.Count;
+                TempData["quantity_khach"] = res.Count;
+                TempData["quantity"] = tour.SLDoanOfTour(res);
                 TempData["doanhthutours"] = res;
                 TempData["thoigianbatdau"] = thoigianbatdau;
                 TempData["thoigianketthuc"] = thoigianketthuc;
                 TempData["ma-tour"] = id_tour;
                 code = Constants.SUCCESS;
                 return Json(new { Code = code, JsonRequestBehavior.AllowGet });
+            }
+            catch (Exception ex)
+            {
+                string message = ex.Message;
+                return Json(new { Message = message, JsonRequestBehavior.AllowGet });
+            }
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult DoanhThuTour6Thang()
+        {
+            try
+            {
+                string code;
+                ArrayList res = tour.DoanhThuTour6Thang();
+                code = Constants.SUCCESS;
+                return Json(new { Code = code, data = res, arr = res, JsonRequestBehavior.AllowGet });
             }
             catch (Exception ex)
             {
